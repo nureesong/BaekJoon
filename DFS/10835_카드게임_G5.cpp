3c984d48f1e20287a65f5c26954c600a46c8b24c
nureesong <76163168+nureesong@users.noreply.github.com>
2022-09-18 15:26:35 +0900

    Mod: 10835_카드게임_G5.cpp

--- DFS/10835_카드게임_G5.cpp
+++ DFS/10835_카드게임_G5.cpp
@@ -1,7 +1,8 @@
 /********************************/
 // [10835] 카드게임 (골드5)
-// 힌트: DFS + 가지치기 잘하기
-// N <= 25 까지 성공
+// DFS + 가지치기. 서브태스크2 까지 성공 (N <= 25)
+// 87 ms: 현재 남아있는 오른쪽 카드를 모두 더해도 maxscore보다 작으면 가지치기
+//  2 ms: BFS에서 많이 사용하던 특정 상태에 대한 최고경험을 바탕으로 한 가지치기 추가
 /********************************/
 #include <iostream>
 using namespace std;
@@ -9,22 +10,30 @@ using namespace std;
 int N;
 int L[25 + 5]; // 1부터 시작
 int R[25 + 5];
-int psum[25 + 5]; // prefix sum for R
+//int psum[25 + 5]; // prefix sum for R
 int maxscore = 0;
 
-void DFS(int Ltop, int Rtop, int score) {
+// chk[l]][r] : 왼쪽카드 l번째, 오른쪽카드 r번째까지 진행된 상황에서 최고 점수
+int chk[25 + 5][25 + 5]; // -1로 초기화
+
+
+void DFS(int Ltop, int Rtop, int score) 
+	// 가지치기 (솔루션): 기존 최고점수보다 높지 않으면 굳이 발전시킬 필요없음
+	if (score <= chk[Ltop][Rtop]) return;
+        chk[Ltop][Rtop] = score;
+
+	// 가지치기 (기존)
+	//if (score + psum[N] - psum[Rtop-1] < maxscore) return;
+	
 	// 종료조건
 	if (Ltop > N || Rtop > N) {
 		if (score > maxscore) maxscore = score;
 		return;
 	}
-
-	// 가지치기
-	if (score + psum[N] - psum[Rtop-1] < maxscore) return;
-
+	
 	// 3가지 경우의 수로 상태 발전
 	if (L[Ltop] > R[Rtop]) DFS(Ltop, Rtop + 1, score + R[Rtop]);
-	else { // L <= R일 때 L을 버려야 하므로 점수를 얻을 가능성이 생기므로 일종의 가지치기!!!
+	else { // L <= R일 때 L의 카드를 버려야 점수를 얻을 가능성이 생긴다.
 		DFS(Ltop + 1, Rtop, score);
 		DFS(Ltop + 1, Rtop + 1, score);
 	}
@@ -37,8 +46,9 @@ void Input() {
 	}
 	for (int i = 1; i <= N; i++) {
 		cin >> R[i];
-		psum[i] = psum[i - 1] + R[i];
+		//psum[i] = psum[i - 1] + R[i];
 	}
+	fill(&chk[1][1], &chk[25][25 + 1], -1);
 }
 
 int main() {
